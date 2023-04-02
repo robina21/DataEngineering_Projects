bfd2a3a4e67c9b7126837c67c20e5246b470ee24
robina <38224300+robina21@users.noreply.github.com>
2023-04-02 12:29:11 -0700

    Create config.py

---
+++ ETL Pipeline for Twitter Data/config.py
@@ -0,0 +1,11 @@
+# Twitter API credentials
+twitter_api_key = 'your_twitter_api_key'
+twitter_api_secret_key = 'your_twitter_api_secret_key'
+twitter_access_token = 'your_twitter_access_token'
+twitter_access_token_secret = 'your_twitter_access_token_secret'
+
+# PostgreSQL database connection details
+db_user = 'your_db_user'
+db_password = 'your_db_password'
+db_host = 'your_db_host'
+db_name = 'your_db_name'
